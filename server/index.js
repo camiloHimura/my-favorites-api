21e69eb30c1cc0a3ba5fac4b15f9e89168ae970f
Camilo Colorado <Camilo_Colorado@epam.com>
2020-01-05 17:28:42 +0100

    config file removed

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const { json, urlencoded } = require("body-parser");
 
 const db = require("./utils/db");
-const config = require("./config");
 const tagRouter = require("./routes/tag");
 const linkRouter = require("./routes/link");
 const errorHandler = require("./utils/errorHandler");
+const port = process.env.PORT || 8080;
 
 const App = express();
 App.use(cors());
@@ -25,7 +25,7 @@ module.exports = {
     async start(){
         try{
             await db.connect();
-            App.listen(config.port, () => console.log(`App Running port ${config.port}`));
+            App.listen(port, () => console.log(`App Running port ${port}`));
         }catch(error){
             console.log("Milo index error", error)
         }
